53a2d0a2bb359dd643b4089ab9c52df5926e732d
claudia njeri <claudianjeri04@gmail.com>
2018-05-12 07:27:50 +0300

    added class News and its parameters

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class DevConfig(Config):
 
     DEBUG = True #we set this to true so that we can run the application on debug mode. This will help with getting roors when they appear.
     
-    NEWS_API_KEY = '8a48624dfbfc41d8a7009f960cccd473'
+    NEWS_API_KEY = '8a48624dfbfc41d8a7009f960cccd473' 
 
     # config_options = {
     #     'development' :DevConfig
