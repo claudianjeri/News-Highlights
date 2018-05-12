2b50b5cb2aea6b80551c973d7a5485431afb214f
claudia njeri <claudianjeri04@gmail.com>
2018-05-12 07:31:12 +0300

    added config classes and defined api keys and base url

--- app/config.py
+++ app/config.py
@@ -1,1 +1,19 @@
- 
+class Config:
+    
+    NEWS_API_KEY = '8a48624dfbfc41d8a7009f960cccd473'
+    NEWS_BASE_URL =' https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLE_NEWS_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+class ProdConfig(Config):
+
+    pass
+
+class DevConfig(Config):
+
+    DEBUG = True #we set this to true so that we can run the application on debug mode. This will help with getting roors when they appear.
+    
+    NEWS_API_KEY = '8a48624dfbfc41d8a7009f960cccd473' 
+
+    # config_options = {
+    #     'development' :DevConfig
+    #     'production' : ProdConfig
+    # }
