33f1a78371d2926642d750f38b768b40d99ccc5a
claudia njeri <claudianjeri04@gmail.com>
2018-05-12 07:33:41 +0300

    added get_news function

--- app/requests.py
+++ app/requests.py
@@ -8,7 +8,20 @@ api_key = app.config['NEWS_API_KEY']
 
 base_url = app.config ["NEWS_API_BASE_URL"]#this how config objects are created.
 
+def get_news(category): #a function that takes news category as an argument
+    get_news_url = base_url.format(category, api_key) #
 
+    with urllib.request.urlopen(get_news_url) as url: #this sends the request as a url
+        get_news_data =url.read()#reads the response then stores it in the get_news_data
+        get_news_response = json.loads(get_news_data)#json response i.e the get_news_data is then converted to a python dictionary
+
+        news_results = None
+
+        if get_news_response['sources']: #checks if the response has any results. 
+            news_results_list = get_news_response['sources']#if there is results then 
+            news_results = process_results(news_results_list)
+
+    return news_results
 
 
 
